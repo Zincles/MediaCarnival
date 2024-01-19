09db365b4154e0d134943f43c9f1b315e66ae3e9
Zincles Down <zincles@outlook.com>
2024-01-20 02:53:49 +0800

    小修小补

--- file_browser/views.py
+++ file_browser/views.py
@@ -10,6 +10,9 @@ def file_browser(request, path=""):
     if os.path.isdir(path):
         raws = os.listdir(path)
 
+        # 对raws进行按名称从A-Z, a-z的排序
+        raws = sorted(raws, key=lambda x: x.lower())
+
         # path是路径。 根据path路径下的文件创建两个字符串数组，字符串分别为path路径下所有子路径/文件的绝对路径
         files = [os.path.join(path, raw) for raw in raws if os.path.isfile(os.path.join(path, raw))]
         folders = [os.path.join(path, raw) for raw in raws if os.path.isdir(os.path.join(path, raw))]
