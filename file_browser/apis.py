15f4dd2d2efaea8a6b31a37b1cf7884bb07d9eaf
Zincles Down <zincles@outlook.com>
2024-01-20 22:46:13 +0800

    Add API endpoint to get folder contents

--- file_browser/apis.py
+++ file_browser/apis.py
@@ -1,18 +1,64 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 import os
 import json
 
 
+## 获取文件夹下的所有文件与文件夹。可指定页数，每页的数量，排序方式，排序顺序
+def api_get_folder(request, path: str):
+    
+    # 获取查询参数
+    page = request.GET.get("page", 1) 
+    page_size = request.GET.get("page_size", 50)  
+    sort = request.GET.get("sort", "name")
+    order = request.GET.get("order", "asc")
+
+    path: str = os.path.join("/", path)  # 待遍历文件夹
+    name: str = os.path.basename(path)  # 所在目录的名称
+
+    if os.path.isdir(path):
+        # 处理names， 对names排序，按A-Z, a-z的顺序，文件夹在先，文件在后
+        _names = os.listdir(path)
+        _names.sort(key=lambda x: x.lower())
+        _names.sort(key=lambda x: os.path.isdir(os.path.join(path, x)), reverse=True)
+
+        # 分页. 从第page页开始，每页page_size个。
+        paginator = Paginator(_names, page_size)
+        names = paginator.get_page(page).object_list
+        is_end = not paginator.page(page).has_next()
+
+        paths = [os.path.join(path, name) for name in names]
+        types = ["folder" if os.path.isdir(path) else "file" for path in paths]
+
+        return HttpResponse(
+            json.dumps(
+                {
+                    "page": page,  # 当前页码
+                    "page_size": page_size,  # 每页的数量
+                    "sort": sort,  # 排序方式
+                    "order": order,  # 排序顺序
+                    "path": path,  # 当前目录
+                    "paths": paths,  # 当前目录下的所有文件与文件夹的路径
+                    "name": name,  # 当前目录的名称
+                    "names": names,  # 当前目录下的所有文件与文件夹的名称
+                    "types": types,  # 当前目录下的所有文件与文件夹的类型
+                    "is_end": is_end,  # 是否到达最后一页
+                }
+            )
+        )
+    else:
+        return HttpResponse("Not a folder" + str(path))
+
+
 ## 获取图像文件
 def api_get_image(request, path: str):
-
     FILE_PATH = os.path.join("/", path)
 
     # 检查FILE_PATH是否有效
     if not os.path.isfile(FILE_PATH):
         return HttpResponse("File not found at: <br>" + str(FILE_PATH))
-    
+
     print(FILE_PATH)
     print("\n")
 
--- file_browser/urls.py
+++ file_browser/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path(route="", view=views.file_browser, name="home"),
     path(route="browser/<path:path>", view=views.file_browser, name="file_browser"),
     path(route="api/get_image/<path:path>", view=apis.api_get_image, name="api_get_image"),
+    path(route="api/get_folder/<path:path>", view=apis.api_get_folder, name="api_get_folder"),
 ]
+
